acddc563fc4c8880dd018a429b7574c9f7bdea39
TIGER MIDORIKAWA <tiger.line108@gmail.com>
2024-01-24 00:17:56 +0900

    ユーザー名の微調整

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -25,7 +25,7 @@
         <h1><a href="/top"><img src="images/logo.png"></a></h1>
             <div id="">
                 <div id="">
-                    <p>〇〇さん<img src="images/arrow.png"></p>
+                    <p>{{ Session('username') }}さん<img src="images/arrow.png"></p>
                 <div>
                 <ul>
                     <li><a href="/top">ホーム</a></li>
@@ -45,7 +45,7 @@
         </div >
         <div id="side-bar">
             <div id="confirm">
-                <p>〇〇さんの</p>
+                <p>{{ Session('username') }}さんの</p>
                 <div>
                 <p>フォロー数</p>
                 <p>〇〇名</p>
