d48513af14f5e59ff44ed724895ffe42c6bb210d
TIGER MIDORIKAWA <tiger.line108@gmail.com>
2024-04-19 19:54:33 +0900

    ユーザー検索

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ Route::get('/top','PostsController@index');
 
 Route::get('/profile','UsersController@profile');
 
-Route::get('/search','UsersController@index');
+Route::get('/search','UsersController@search');
 
 Route::get('/follow-list','PostsController@followList');
 Route::get('/follower-list','PostsController@followerList');
